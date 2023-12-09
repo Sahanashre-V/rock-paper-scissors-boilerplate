a273a3fb0dccd9a360059a1c5c531b93634e7abd
V.Sahanashre <sahanashre.v@kalvium.community>
2023-12-09 09:30:02 +0530

    Added

--- game.js
+++ game.js
@@ -4,7 +4,6 @@ let comp_score=0;
 let userScoreSpan = document.getElementById("userScore");
 let compScoreSpan = document.getElementById("compScore");
 
-
 logos.forEach(item => {
     item.addEventListener("click", function (event) {
         let clickedId = event.target.id;
@@ -15,32 +14,24 @@ logos.forEach(item => {
         if (randnums === 0) {
             img.src = "./assets/rock-hand.png";
             img.style.height="250px"
-            // console.log(randnums)
         } else if (randnums === 1) {
             img.src = "./assets/paper-hand.png";
             img.style.height="250px"
-            // console.log(randnums)
         } else {
             img.src = "./assets/scissors-hand.png";
             img.style.height="250px"
-            // console.log(randnums)
         }
 
         let lists = ["rock","paper","scissors"]
 
         let user_choice= clickedId;
         let comp_choice = randnums;
-        console.log(user_choice)
-        console.log(randnums)
 
         function determine_winner(user_choice,comp_choice){
            if (user_choice === lists[comp_choice]){
-            // my_score+=0
-            // comp_score+=0
             return "Match is a tie"
-            // determine_winner(user_choice,comp_choice)
-            
            }
+
            else if (
             (user_choice==="rock" && lists[comp_choice]==="scissors")||
             (user_choice==="paper" && lists[comp_choice]==="rock") ||
@@ -49,74 +40,56 @@ logos.forEach(item => {
            my_score+=1;
            userScoreSpan.innerText=my_score;
            if(my_score===5){
-            let ptag = document.createElement("p")
+            let ptag = document.getElementById("ptag")
             ptag.innerText="You Won the Game";
+
             let logoid = document.getElementById("logoid");
             logoid.style.visibility="hidden";
-            ptag.style.color= "white";
-            ptag.style.textAlign="center"
-            document.body.appendChild(ptag);
-            let playagain = document.createElement("button");
+            // document.body.appendChild(ptag);
+
+            let playagain = document.getElementsByTagName("button")[0];
                 playagain.innerText="Play Again";
-                playagain.style.color="white";
-                playagain.style.backgroundColor="#f7bb3d";
-                // playagain.style.margin="700px";
-                playagain.style.marginTop="-950px";
-                playagain.style.borderRadius="5px";
-                playagain.style.padding="10px";
-                playagain.style.fontSize="30px";
-                playagain.style.fontWeight="700";
-                playagain.style.marginLeft="660px";
+                playagain.style.display="block";
                 playagain.addEventListener("click",function(){
                     window.location.href="./game.html"
                 })
-                document.body.appendChild(playagain);
+                // document.body.appendChild(playagain);
         }
            return "You won!"
            }
+
            else{
-            // window.location.href="./index.html";
             comp_score+=1;
             compScoreSpan.innerText=comp_score;
             if(comp_score===5){
-                let ptag = document.createElement("p")
+                let ptag = document.getElementById("ptag")
                 ptag.innerText="You Lose the Game";
+
                 let logoid = document.getElementById("logoid");
                 logoid.style.visibility="hidden";
-                ptag.style.color= "white";
-                ptag.style.textAlign="center"
-                document.body.appendChild(ptag);
-                let playagain = document.createElement("button");
+
+                let playagain = document.getElementsByTagName("button")[0];
                 playagain.innerText="Play Again";
-                playagain.style.color="white";
-                playagain.style.backgroundColor="#f7bb3d";
-                playagain.style.marginLeft="660px"
-                playagain.style.marginTop="-950px";
-                playagain.style.borderRadius="5px";
-                playagain.style.padding="10px";
-                playagain.style.fontSize="30px";
-                playagain.style.fontWeight="700";
+                playagain.style.display="block";
                 playagain.addEventListener("click",function(){
                     window.location.href="./game.html"
                 })
 
-                document.body.appendChild(playagain)
             }
             return "You lose!"
            }
 
         }
         
-
         let output=determine_winner(user_choice,comp_choice)
-        console.log(output);
-        console.log("myscore",my_score);
-        console.log("compsocre",comp_score);
+        // console.log(output);
+        // console.log("myscore",my_score);
+        // console.log("compsocre",comp_score);
 
         complier.innerHTML = "";
-        complier.appendChild(img);
+        complier.appendChild(img);//adding image to the complier div
 
-        console.log("ID",clickedId)
+        // console.log("ID",clickedId)
     });
 });
 
@@ -150,8 +123,8 @@ function displayImage(image) {
 
 function hideAllImages() {
     const allImages = document.querySelectorAll('.game-image');
-    allImages.forEach(image => {
-        image.style.display = 'none';
+    allImages.forEach(otherimage => {
+        otherimage.style.display = 'none';
     });
 }
 
